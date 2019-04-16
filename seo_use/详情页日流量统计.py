1c1b5025a382002c7722f8a55bf0a5e06fccd766
caizhiwei998 <45473322+caizhiwei998@users.noreply.github.com>
2019-04-16 16:14:55 +0800

    Update 详情页日流量统计.py

--- seo_use/详情页日流量统计.py
+++ seo_use/详情页日流量统计.py
@@ -17,10 +17,10 @@ class sv_soil_pv_web:
     def dbconn_nglog(self):
         '''日志数据库连接'''
         dbconn = pymysql.connect(
-            host="********",
-            user="********",
-            password="********",
-            db="nginx_log",
+            host="******",
+            user="******",
+            password="******",
+            db="******",
             port=3306,
             charset='utf8'
         )
@@ -30,10 +30,10 @@ class sv_soil_pv_web:
     def dbconn_center(self):
         '''服务中心数据库连接'''
         dbconn = pymysql.connect(
-            host="********",
-            user="********",
-            password="********",
-            db="center",
+            host="******",
+            user="******",
+            password="******",
+            db="******",
             port=3306,
             charset='utf8'
         )
@@ -65,11 +65,11 @@ class sv_soil_pv_web:
 
     def get_sview_data(self):
         '''获取服务中心土地[url,date]列表'''
-        sview_data_list=[]
         for date in self.date_list:
             sql= '''
             SELECT url,case sitetype when 'tuliu_pc' then 'pc' when 'tuliu_wap' then 'wap' end
             from %s where url REGEXP('s-view-') and time BETWEEN '%s 00:00:00' and '%s 23:59:59'
+            and uag != 'mozilla/5.0 (compatible; baiduspider-render/2.0; +http://www.baidu.com/search/spider.html)'            
             ''' % (self.nglog_table,date, date)
             self.cur_nglog.execute(sql)
             url_tuple = self.cur_nglog.fetchall()
@@ -77,7 +77,6 @@ class sv_soil_pv_web:
                 url_date_list = [url[0],url[1],date]
                 print(url_date_list)
                 yield  url_date_list #[url,src,date]
-
 
 
     def get_soilid_date(self):
@@ -111,23 +110,62 @@ class sv_soil_pv_web:
             date = svid_date[2]
             i+=1
             print('NO'+str(i)+'_sv_id='+str(sv_id))
-            sql_find_sv = "select sv_id from sv_data_analyse.sv_soil_pv_daily where date = '%s' and sv_id = %s"%(date,sv_id)
+            sql_find_sv = "select sv_id from sv_data_analyse.sv_soilpv_daily " \
+                          "where date = '%s' and sv_id = %s and src = '%s' "%(date,sv_id,src)
             self.cur_nglog.execute(sql_find_sv)
             find_sv=self.cur_nglog.fetchall()
             if len(find_sv)>0:
-                sql_update_sv="update sv_data_analyse.sv_soil_pv_daily set soil_pv=soil_pv+1 " \
-                              "where date='%s' and sv_id=%s"%(date,sv_id)
+                sql_update_sv="update sv_data_analyse.sv_soilpv_daily set soil_pv=soil_pv+1 " \
+                              "where date='%s' and sv_id=%s and src = '%s' "%(date,sv_id,src)
                 self.cur_nglog.execute(sql_update_sv)
             else:
-                sql_insert_sv="insert into sv_data_analyse.sv_soil_pv_daily values(%s,%s,'1',%s)"
+                sql_insert_sv="insert into sv_data_analyse.sv_soilpv_daily values(%s,%s,'1',%s)"
                 self.cur_nglog.execute(sql_insert_sv,(date,sv_id,src))
 
 
 
+class sv_soil_pv_app(sv_soil_pv_web):
+
+    def __init__(self,datestart,dateend,nglog_table):
+        sv_soil_pv_web.__init__(self, datestart, dateend, nglog_table)
+
+
+    def get_sview_data(self):
+        '''获取土地[soil_id-(1,2),date]列表(APP日志中-1为服务中心-2为主站)'''
+        for date in self.date_list:
+            sql= '''
+            SELECT qt,dk from %s where time BETWEEN '%s 00:00:00' and '%s 23:59:59' and xw= '浏览-土地供应详情'           
+            ''' % (self.nglog_table,date, date)
+            self.cur_nglog.execute(sql)
+            url_tuple = self.cur_nglog.fetchall()
+            for qt in url_tuple:
+                qt_date_list = [qt[0],qt[1],date]
+                print(qt_date_list)
+                yield  qt_date_list #[soil_id-(1,2),src,date]
+
+
+    def get_soilid_date(self):
+        '''获取土地id'''
+        pattern = re.compile(r'(\d+)-(\d+)')
+        for qt_date in self.get_sview_data():
+            m_qt = pattern.search(qt_date[0])
+            if m_qt != None:
+                if m_qt.group(2) == '1':
+                    soil_id = m_qt.group(1)
+                    sid_date_list=[soil_id,qt_date[1],qt_date[2]]
+                    yield sid_date_list #[soil_id,src,date]
+
+
 if __name__ == '__main__':
-    test=sv_soil_pv_web('2019-04-01','2019-04-03','nglog_pcwap')
-    test.update_sv_data()
-    test.close_dbconn()
+    web03=sv_soil_pv_web('2019-03-01','2019-03-31','nglog_pcwap_201903')
+    web03.update_sv_data()
+    web03.close_dbconn()
+
+    app03=sv_soil_pv_app('2019-03-01','2019-03-31','nglog_app')
+    app03.update_sv_data()
+    app03.close_dbconn()
+
+
 
 
 
